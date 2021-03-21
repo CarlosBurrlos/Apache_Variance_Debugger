6222fdd9c8e2eeba18843f05354e2cc4c8e78492
CarlosBurrlos <68665299+CarlosBurrlos@users.noreply.github.com>
2021-03-21 13:19:07 -0400

    Renamed

--- Debugger/Debug.cpp
+++ Debugger/Debug.cpp
@@ -1,22 +1,32 @@
 #include "../Headers/All.h"
 
+#include    <iostream>
 
+const char * _illegal_usage = "Debug: illegal option";
+const char * usage = "usage: Debug [(FILE_NAME)]";
 
-void    __usage();
+void    __usage(int, char **);
 
 int main(int argc, char * argv[]) {
 
+    /*
     if (argc == 0 || argc > 1) {
-        __usage();
+        __usage(argc, argv);
+        return -1;
     }
+     */
 
-    auto *s = new Scanner("../tmt.txt");
+    auto *s = new Scanner("../Tests/test1.txt");
     s->readWord();
 
     return 0;
 }
 
-void __usage() {
-
-
+void __usage(int argc, char ** argv) {
+    std::cout << _illegal_usage << ' ';
+    for (; argc > 0; argc--) {
+        std::cout << argv[argc];
+    }
+    std::cout << '\n';
+    std::cout << usage << '\n';
 }
