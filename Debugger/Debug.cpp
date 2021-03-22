746730c876c89f2ff2f1ef2639f32e1bd94b808c
CarlosBurrlos <68665299+CarlosBurrlos@users.noreply.github.com>
2021-03-22 14:12:06 -0400

    Driver Code Now Complete

--- Debugger/Debug.cpp
+++ Debugger/Debug.cpp
@@ -2,25 +2,36 @@
 
 #include    <iostream>
 
+Scanner * s = new Scanner("../Tests/test0.txt");
+Parser * p = new Parser(s);
+
 const char * _illegal_usage = "Debug: illegal option";
 const char * usage = "usage: Debug [(FILE_NAME)]";
 
 void    __usage(int, char **);
 
 int main(int argc, char * argv[]) {
-
     /*
     if (argc == 0 || argc > 1) {
         __usage(argc, argv);
         return -1;
     }
-     */
+    */
+
+    while (s->readWord() != END) {
+        p->parse();
+    }
+
+    /*Debugging*/
+    /*
     std::string_view debug_str;
     auto *s = new Scanner("../Tests/test1.txt");
     while (1) {
         if (s->readWord() == 3)
             break;
     }
+    */
+
     return 0;
 }
 
