11b0e456270fb1cc70c9313f344a1b5a343f037a
CarlosBurrlos <68665299+CarlosBurrlos@users.noreply.github.com>
2021-03-27 12:05:33 -0400

    Once again I am asking for your financial support

--- Debugger/Parser.cpp
+++ Debugger/Parser.cpp
@@ -1,24 +1,20 @@
 #include "../Headers/All.h"
 
-#include <cstring>
 #include <unordered_set>
 
-const char * SEMICOL         = ";";
-const char * LPAREN          = "(";
-const char * RPAREN          = ")";
-
-#define  equals(t, tokens, n) ({\
-    bool retVal;                \
-    for (;n >= 0; n--)          \
-        if (tokens[n] == t) {   \
-          retVal = true;        \
-          retVal;               \
-        }                       \
-    retVal = false;             \
-    retval;\
-})
+//========================= Helpers ==========================
+bool Parser::checkAndConsume(int token) {
+    if (Token == token) { scanner->readWord(); return true; }
+    else return false;
+}
+bool Parser::check(int token) {return (Token == token);}
+void Parser::consume() {scanner->readWord();}
+void Parser::consumeLine() {scanner->readLine();}
+//============================================================
 
 bool Parser::parse() {
+    return parsePreProc();
+    /*
     if (!parsePreProc())
         return 0;
     else if (!parseProto())
@@ -26,23 +22,24 @@ bool Parser::parse() {
     else if (!parseScope())
         return 0;
     return 1;
+     */
 }
 
-//Gets a the new func string and creates func itm and stores it
+//TODO::Get rid of this one
 bool Parser::parseScope() {
 
     //func->retType scopeName(args){ funcBody }
-
-    if (checkAndConsume(RETINT) || checkAndConsume(RETVOID)) {
-        std::string_view scopeName = scanner->getCurrStr();
-        Scope * s = new Scope(scopeName);
-        consume(); consume();
-        if (checkAndConsume(SCOPE))
-            enterScope(s);
-        else return false;
+    int retType;
+    if ((retType = checkAndConsume(RETINT)) || (retType = checkAndConsume(RETVOID))) {
+        //TODO::Set Scope's retType using retType
+        Scope *s = new Scope(scanner->getCurrStr());
+        consume();
+        parseArgs(s);
+        consume();
+        enterScope(s);
+        consume();
         if (!parseFuncBody()) {
             //Could Be No Func Calls in this Scope
-            return false;
         }
         consume();
         exitScope();
@@ -51,18 +48,43 @@ bool Parser::parseScope() {
     return false;
 }
 
+//Just need to check if scope, if true, then consume and call func bod
+
+bool Parser::parseFunc() {
+    if (checkAndConsume(RETVOID) || checkAndConsume(RETINT)) {
+        if (check(FUNC)) {
+            std::string_view name = scanner->getCurrStr();
+            Func * f;
+            if (allScopes.find(name) != allScopes.end()) {
+                f = new Func(name);
+                allFuncs.insert( {name, f} );
+            }
+            else {
+                f = allFuncs[name];
+            }
+            consume(); parseArgs(f); consume();
+            if (check(SCOPE)) {
+                //TODO::Write a func to scope method
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+//Just need to call consume() if true
+
 bool Parser::parseFuncBody() {
     //funcbody -> funCall funcBody
     if (check(FUNC)) {
         while(parseFuncCall())
             ;
+        return 1;
     }
-    else {
-        return 0;
-    }
-    return 1;
+    return 0;
 }
 
+//TODO::Test more - Good so far
 bool Parser::parseFuncCall() {
     //funcCall -> funcName ( args );
     if (!check(FUNC))
@@ -80,27 +102,33 @@ bool Parser::parseFuncCall() {
     return 1;
 }
 
+//TODO::Test more - Good so far
 bool Parser::parsePreProc() {
     //preproc -> # preprocprime word;
     if (check(PREPROC)) {
         consumeLine();
+        parsePreProc();
         return true;
     }
     return false;
 }
 
+//TODO::Get rid of this one
 bool Parser::parseProto() {
-
+    if (check(RETVOID) || check(RETINT))
+        consume();
     if (check(FUNC)) {
         std::string_view FuncName = scanner->getCurrStr();
         Func * f = new Func(FuncName);
         std::pair<std::string_view, Func *> pair(f->getName(), f);
         allFuncs.insert(pair);
-        parseArgs(f);
-        consume();
+        scanner->readLine();
+        return true;
     }
     return false;
 }
+
+
 
 bool Parser::parseArgs(Func * f) {
     if (check(ARGS)) {
@@ -110,21 +138,10 @@ bool Parser::parseArgs(Func * f) {
     return false;
 }
 
-bool Parser::checkAndConsume(int token) {
-    if (Token == token) {
-        scanner->readWord();
+bool Parser::parseArgs(Scope * s) {
+    if (check(ARGS)) {
+        s->setArgs(&scanner->file[scanner->wf_idx], scanner->getCurrStrSize());
         return true;
     }
     return false;
-}
-
-bool Parser::check(int token) {
-    return (Token == token);
-}
-
-void Parser::consume() {
-    scanner->readWord();
-}
-void Parser::consumeLine() {
-    scanner->readLine();
 }
