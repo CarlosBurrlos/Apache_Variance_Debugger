f61f22eda87bf5568cf37f0ecd772f5713554c41
CarlosBurrlos <68665299+CarlosBurrlos@users.noreply.github.com>
2021-03-31 14:06:17 -0400

    Had to hardcode printf symbol

--- Debugger/Debug.cpp
+++ Debugger/Debug.cpp
@@ -3,7 +3,7 @@
 
 #include    <iostream>
 
-Scanner * s = new Scanner("../Tests/test0.txt");
+Scanner * s = new Scanner("../Tests/test1.txt");
 Parser * p = new Parser(s);
 
 const char * _illegal_usage = "Debug: illegal option";
@@ -19,9 +19,17 @@ int main(int argc, char * argv[]) {
     }
     */
     freopen("../Output/out1.txt", "w", stdout);
-    Func * f = new Func("pf(");
-    allFuncs.insert( {"pf(", f} );
+    Func * f = new Func("printf(");
+    allFuncs.insert( {"printf(", f} );
     p->parse();
+
+    for (std::pair<std::string_view, Scope*> s_pair : allScopes) {
+        std::cout << s_pair.first << '\n';
+        for (std::pair<std::string_view, Func*> f_pair : s_pair.second->getFuncs()) {
+            std::cout << '\t' << f_pair.first << '\n';
+        }
+    }
+
     fclose(stdout);
     return 0;
 }
