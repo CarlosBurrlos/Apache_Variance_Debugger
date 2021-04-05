6e4b1d24f2a9746c127c196e1fbb182317a229d2
CarlosBurrlos <68665299+CarlosBurrlos@users.noreply.github.com>
2021-04-05 16:28:36 -0400

    Cmake

---
+++ cmake-build-debug/CMakeFiles/408Debugger.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/los/Documents/cs408/408Debugger/Debugger/Debug.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/Debug.cpp.o"
+  "/Users/los/Documents/cs408/408Debugger/Debugger/Func.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/Func.cpp.o"
+  "/Users/los/Documents/cs408/408Debugger/Debugger/Invar.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/Invar.cpp.o"
+  "/Users/los/Documents/cs408/408Debugger/Debugger/NFAs.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/NFAs.cpp.o"
+  "/Users/los/Documents/cs408/408Debugger/Debugger/Parser.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/Parser.cpp.o"
+  "/Users/los/Documents/cs408/408Debugger/Debugger/Scanner.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/Scanner.cpp.o"
+  "/Users/los/Documents/cs408/408Debugger/Debugger/Scope.cpp" "/Users/los/Documents/cs408/408Debugger/cmake-build-debug/CMakeFiles/408Debugger.dir/Debugger/Scope.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/408Debugger.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "408Debugger"
+  "408Debugger.pdb"
+  "CMakeFiles/408Debugger.dir/Debugger/Debug.cpp.o"
+  "CMakeFiles/408Debugger.dir/Debugger/Func.cpp.o"
+  "CMakeFiles/408Debugger.dir/Debugger/Invar.cpp.o"
+  "CMakeFiles/408Debugger.dir/Debugger/NFAs.cpp.o"
+  "CMakeFiles/408Debugger.dir/Debugger/Parser.cpp.o"
+  "CMakeFiles/408Debugger.dir/Debugger/Scanner.cpp.o"
+  "CMakeFiles/408Debugger.dir/Debugger/Scope.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/408Debugger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
